acfd31ab1abffe7ca19024fca4a7f4af2ca7a600
jim@vheavy.com <jim@vheavy.com>
2025-07-15 09:31:28 -0700

    More change to scripts for the new dir structure

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -3,7 +3,7 @@ CATF=../esp32/cat.usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A
 
 cd `dirname $0`
 #while sleep 1; do 
-	grep DONE stash/*.output | sort -n | tail -`tput lines`
+	grep DONE ../stash/*.output | sort -n | tail -`tput lines`
 	echo $(( $(date '+%s') - $(date -r "${CATF}" '+%s') ))
 #done
 tail -1 ${CATF}
