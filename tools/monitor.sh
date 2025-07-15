f7fbe6aef1c280f30b4ab8952009e67be2ff0425
jim@vheavy.com <jim@vheavy.com>
2025-07-15 09:28:24 -0700

    Fix scripts for new dir structure

--- tools/monitor.sh
+++ tools/monitor.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+CATF=./esp32/cat.usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A8-if00.out
+
 cd `dirname $0`
 #while sleep 1; do 
 	grep DONE stash/*.output | sort -n | tail -`tput lines`
-	echo $(( $(date '+%s') - $(date -r cat.usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A8-if00.out '+%s') ))
+	echo $(( $(date '+%s') - $(date -r ${CATF} '+%s') ))
 #done
-tail -1 cat.usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A8-if00.out
+tail -1 ${CATF}
 sleep 3
-tail -1 cat.usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A8-if00.out
+tail -1 ${CATF}
 
 
 
