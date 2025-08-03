78566b6a2844e3cb22e926dae35bb50143b2c76f
jim@vheavy.com <jim@vheavy.com>
2025-08-03 16:22:55 -0700

    Package git info with build.tgz before shipping it to miner6

--- tools/deploy-idf.sh
+++ tools/deploy-idf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 #
 idf.py build
-find build -name *.bin | xargs tar czvf build/build.tgz 
+echo "$(git describe --abbrev=6 --dirty --always)" > build/git.version.txt
+git diff > build/git.diff
+find build -name *.bin | xargs tar czvf build/build.tgz build/git.*  
 scp build/build.tgz tun-miner6:/tmp/build.tgz
 ssh tun-miner6 "cd /home/jim/tmp/ && tar xzvf /tmp/build.tgz"
 
