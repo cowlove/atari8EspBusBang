b5e0a4e6936fb727dfa10bac0af86fef17da0f54
Jim Evans <jim@vheavy.com>
2025-08-04 13:49:59 -0700

    Changes to hw-idf.sh

--- tools/hw-idf.sh
+++ tools/hw-idf.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-# Build and run on hardware
+#!/bin/bash -ex
 PORT=/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A8-if00
-
 TAG=`date +%Y%m%d.%H%M%S`
 
-mkdir stash/
-
+mosquitto_pub -h 192.168.68.137 -t cmnd/tasmota_71D51D/POWER -m OFF
+mosquitto_pub -h 192.168.68.137 -t cmnd/tasmota_71D51D/POWER -m OFF
 mosquitto_pub -h 192.168.68.137 -t cmnd/tasmota_71D51D/POWER -m OFF 
-git diff > stash/${TAG}.git_diff
-git describe  --abbrev=8 --always > stash/${TAG}.git_commit 
-idf.py build flash
+sleep 2
+#(cd ~/tmp/build &&~/src/arduino-esp32/tools/esptool/esptool --chip esp32s3 -p ${PORT} -b 460800 --before=default_reset --after=hard_reset write_flash --flash_mode dio --flash_freq 80m --flash_size 8MB 0x0 bootloader/bootloader.bin 0x10000 atari8EspBusBang.bin 0x8000 partition_table/partition-table.bin 0xe000 ota_data_initial.bin)
+idf.py build flash -p ${PORT}
+#cp ~/tmp/build/git.diff ./stash/${TAG}.git_diff
+#cp ~/tmp/build/git.version.txt ./stash/${TAG}.git_commit
 ( sleep 3 && mosquitto_pub -h 192.168.68.137 -t cmnd/tasmota_71D51D/POWER -m ON ) &
 touch start.ts
-make -C esp32 PORT=${PORT} cat | cat_until DONE
-cp ./esp32/cat.`basename ${PORT}`.out ./stash/${TAG}.output
+(while sleep .1; do if [ -c ${PORT} ]; then stty -F ${PORT} -echo raw; cat ${PORT}; fi; done) | cat_until DONE | tee ./stash/${TAG}.output
+
 
 
