0ab809d4313f3200981099b7f9a8ad8174ac99bd
jim@vheavy.com <jim@vheavy.com>
2025-08-07 06:38:11 -0700

    Add linker fragment script to place entire main component in IRAM

--- main/atari8EspBusBang.cpp
+++ main/atari8EspBusBang.cpp
@@ -359,7 +359,7 @@ DRAM_ATTR static const int psram_sz = 1 * 1024 * 1024;
 DRAM_ATTR uint32_t *psram;
 DRAM_ATTR uint32_t *psram_end;
 
-DRAM_ATTR static const int testFreq = 1.8 * 1000000;//1000000;
+DRAM_ATTR static const int testFreq = 1.78 * 1000000;//1000000;
 DRAM_ATTR static const int lateThresholdTicks = 180 * 2 * 1000000 / testFreq;
 static const DRAM_ATTR uint32_t halfCycleTicks = 240 * 1000000 / testFreq / 2;
 
