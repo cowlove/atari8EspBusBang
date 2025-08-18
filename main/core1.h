ef6c9064f99d0f10f7e682dde00f88f12806d2ba
Jim Evans <jim@vheavy.com>
2025-08-18 11:32:43 -0700

    log max bmon trace

--- main/core1.h
+++ main/core1.h
@@ -115,7 +115,7 @@ static const DRAM_ATTR struct {
    bool timingTest    = 0;
    bool bitResponse   = 0;
    bool core0Led      = 0; // broken, PBI loop overwrites entire OUT1 register including 
-   int dumpPsram      = 10000;
+   int dumpPsram      = INT_MAX;
    bool forceMemTest  = 0;
    bool tcpSendPsram  = 0;
    bool histogram     = 1;
