a82bde02baaf6e5d752ec3b5e3b10ea3ef308c84
jim@vheavy.com <jim@vheavy.com>
2025-08-30 07:09:21 -0700

    Forgot to add extMem.*

---
+++ main/extMem.cpp
@@ -0,0 +1,35 @@
+#include "extMem.h"
+
+
+IRAM_ATTR uint8_t *ExtBankPool::getBank(int b) {
+    b = premap[b]; 
+    if (b < 0) return NULL;
+    if (!isSRAM(b)) {
+        int evict = recency[sramBanks - 1];
+        uint8_t *newspare = banks[b];
+        this->memcpy(spare, banks[evict], bankSz);
+        this->memcpy(banks[evict], banks[b], bankSz);
+        banks[b] = banks[evict];
+        banks[evict] = spare;
+        spare = newspare; 
+        // evict was at the bottom of the list, move whole list down and insert b at top 
+        for(int i = sramBanks - 1; i > 0; i--) {  
+            recency[i] = recency[i - 1];
+        }
+        recency[0] = b;
+        evictCount++;
+    } else {                 
+        // find b and insert it at the top of the recency list
+        for(int i = 0; i < sramBanks; i++) { 
+            if (recency[i] == b) {
+                for(int j = i - 1; j >= 0; j--) {
+                    recency[j + 1] = recency[j];
+                }
+                break;
+            }
+        }
+        recency[0] = b;
+    }
+    swapCount++;
+    return banks[b];
+}
---
+++ main/extMem.h
@@ -0,0 +1,69 @@
+#include <inttypes.h>
+#include <string.h>
+#include "esp_attr.h"
+#include "esp_heap_caps.h"
+
+class ExtBankPool {
+    int totalBanks, sramBanks;
+    uint8_t **banks;
+    int *recency;
+    uint8_t *spare = NULL;
+    DRAM_ATTR static const int bankSz = 0x4000;
+    int premap[16] = {};
+public: 
+    int evictCount = 0, swapCount = 0;
+    void init(int n, int sram) {
+        totalBanks = n;
+        sramBanks = sram;
+        banks = new uint8_t *[n];
+        recency = new int[n];
+        // allocate the first banks in DRAM 
+        for(int n = 0; n < sramBanks; n++) { 
+            banks[n] = (uint8_t *)heap_caps_malloc(bankSz, MALLOC_CAP_INTERNAL);
+            bzero(banks[n], bankSz);
+            recency[n] = n;
+        }
+        // allocate the rest in PSRAM
+        for(int n = sramBanks; n < totalBanks; n++) { 
+            banks[n] = (uint8_t *)heap_caps_malloc(bankSz, MALLOC_CAP_SPIRAM);
+            bzero(banks[n], bankSz);
+        }
+        if (totalBanks > sramBanks) {
+            spare = (uint8_t *)heap_caps_malloc(bankSz, MALLOC_CAP_SPIRAM);
+            bzero(spare, bankSz);
+        }
+        mapStockXE();
+    }
+
+     
+    void mapStockXE() {  
+        for(int i = 0; i < 4; i++) { 
+            premap[i + 0b0000] = i;
+            premap[i + 0b0100] = i;
+            premap[i + 0b1000] = i;
+            premap[i + 0b1100] = i;
+        }
+    }
+
+    void mapRambo256() {
+        for(int i = 0; i < 4; i++) premap[i] = -1;
+        for(int i = 4; i < 16; i++) premap[i] = i - 4;
+    }
+    void mapCompy192() {
+        for(int i = 0; i < 4; i++) { 
+            premap[i + 0b0000] = i;
+            premap[i + 0b0100] = i;
+            premap[i + 0b1000] = i + 4;
+            premap[i + 0b1100] = i + 4;
+        }
+    }
+    
+    IRAM_ATTR inline void memcpy(uint8_t *dst, uint8_t *src, int len) { 
+        for(int n = 0; n < len; n++) dst[n] = src[n];
+    }
+    IRAM_ATTR inline bool isSRAM(int b) {
+        static const DRAM_ATTR uint8_t *cutoff = (uint8_t *)0x3f000000;
+        return banks[b] > cutoff;
+    }
+    uint8_t *getBank(int b);
+};
