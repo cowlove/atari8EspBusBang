10feeb67601e184bdf42c560d3b3033a0b933315
Jim Evans <jim@vheavy.com>
2025-08-31 14:21:50 -0700

    Add blank line to showCrashScreens

--- tools/showCrashScreens.sh
+++ tools/showCrashScreens.sh
@@ -7,6 +7,7 @@ if [ "$TAIL" == "" ]; then TAIL=20; fi
 CRASHES=`ls -1tr stash/*.output | tail -${TAIL} | xargs egrep -l "DONE(.*)-[0-9]"`
 for f in $CRASHES; do
 	echo
+	echo
 	echo less `pwd`/$f
 	egrep "(SCREENB)|(DONE)" $f
 	echo -n "00FF reads in bmon: "
