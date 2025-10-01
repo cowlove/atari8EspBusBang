0f568aff9a2be84115ddec59f5662f7ff5e9e4f9
Jim Evans <jim@vheavy.com>
2025-10-01 10:54:34 -0700

    Tweaked number of nops to get code to run.  Still don't understand the restructuring delays

--- main/core1.cpp
+++ main/core1.cpp
@@ -54,7 +54,7 @@ void iloop_pbi() {
         uint32_t pinDrMask = pinDriveMask;
 
         // Timing critical point #1: >= 43 ticks after clock edge until read of address/control lines
-        ASM("nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;nop;");
+        ASM("nop;nop;nop;nop;nop;nop;nop;nop;nop;");
         r0 = REG_READ(GPIO_IN_REG);
         PROFILE1(XTHAL_GET_CCOUNT() - tscFall); 
 
