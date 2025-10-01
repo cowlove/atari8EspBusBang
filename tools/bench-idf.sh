fdb04e3336c5c5914c9e1c4ea522832914c4950b
jim@vheavy.com <jim@vheavy.com>
2025-10-01 10:05:14 -0700

    Don't update timings.last in bench.sh

--- tools/bench-idf.sh
+++ tools/bench-idf.sh
@@ -11,7 +11,7 @@ idf.py --ccache -DPROFILEMODE=${PTEST} -DTEST_SEC=${TEST_SEC} build flash
 make -C main PORT=${PORT} cat | cat_until "DONE" |  tee out/cat.out 
 
 touch out/timing${PTEST}.txt
-mv out/timing${PTEST}.txt out/timing${PTEST}.last.txt
+#mv out/timing${PTEST}.txt out/timing${PTEST}.last.txt
 grep HIST out/cat.out  > out/timing${PTEST}.txt
 
 ./tools/compareTimings.sh ./out/timing${PTEST}.last.txt ./out/timing${PTEST}.txt 
