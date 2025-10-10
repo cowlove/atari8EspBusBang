8f82b10256f94641816baddbc3cb7a8628809fb8
Jim Evans <jim@vheavy.com>
2025-10-10 11:22:21 -0700

    update bench.sh from master

--- tools/bench-idf.sh
+++ tools/bench-idf.sh
@@ -1,17 +1,29 @@
 #!/bin/bash -ex 
+cd `dirname $0`/..
+. ./tools/config
+
+if [ "$1" == "-u" ]; then
+    shift;
+    UPDATE_REF=true
+else 
+    UPDATE_REF=false
+fi
+
 PTEST="$1"
 TEST_SEC="$2"
 if [ "$PTEST" == "" ]; then PTEST=1; fi
 if [ "$TEST_SEC" == "" ]; then TEST_SEC=10; fi
 
-#PORT=/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_B4\:3A\:45\:A5\:C4\:2C-if00
-PORT=/dev/ttyACM0
+mosquitto_pub -h 192.168.68.137 -t cmnd/${TAS}/POWER -m OFF
+mosquitto_pub -h 192.168.68.137 -t cmnd/${TAS}/POWER -m OFF
+mosquitto_pub -h 192.168.68.137 -t cmnd/${TAS}/POWER -m OFF 
+
 ./tools/updateGitH.sh
-idf.py --ccache -DPROFILEMODE=${PTEST} -DTEST_SEC=${TEST_SEC} build flash
-make -C main PORT=${PORT} cat | cat_until "DONE" |  tee out/cat.out 
+idf.py --ccache -DPROFILEMODE=${PTEST} -DTEST_SEC=${TEST_SEC} -p ${PORT} app flash
+make -C main PORT=${PORT} cat | cat_until "DONE" ${PORT} |  tee out/cat.out 
 
 touch out/timing${PTEST}.txt
-#mv out/timing${PTEST}.txt out/timing${PTEST}.last.txt
 grep HIST out/cat.out  > out/timing${PTEST}.txt
+if $UPDATE_REF; then cp out/timing${PTEST}.txt out/timing${PTEST}.last.txt; fi 
 
 ./tools/compareTimings.sh ./out/timing${PTEST}.last.txt ./out/timing${PTEST}.txt 
