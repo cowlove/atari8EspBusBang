e6955beb169bb8ec93222b25a49ab049be42e3eb
Jim Evans <jim@vheavy.com>
2025-10-10 17:59:58 -0700

    Add UBSA to config, use all 24 bits of cap, analyzer looks at writes and non-extsel reads

--- tools/analyze.py
+++ tools/analyze.py
@@ -8,35 +8,48 @@ ccount = 0
 clk = 3
 extsel = 2
 d0 = 0
+d1 = 23
+rw = 20
+a0 = 11
+a1 = 12
+a2 = 10
+a3 = 13
 
 extselTL = -1 
 extselTH = -1
 dTE = -1 
 dTL = -1
 
-hist = np.zeros((5,100))
+hist = np.zeros((8,100))
 lineNo = 0
 
 for line in d:
     if lastLine is None: lastLine = line
     if lastLine[clk] == 1 and line[clk] == 0:
-        if lineNo > 1:
+        if lineNo > 2:
             #print ("%3d %3d %3d" % (extselTH, extselTL, dT))
             if (extselTH >= 0 and extselTH < 100): hist[0][extselTH] = hist[0][extselTH] + 1
             if (extselTL >= 0 and extselTL < 100): hist[1][extselTL] = hist[1][extselTL] + 1
             #if (dTE >= 0 and dTE < 100): hist[2][dTE] = hist[2][dTE] + 1
             #if (dTL >= 0 and dTL < 100): hist[3][dTL] = hist[3][dTL] + 1
-            if (ccount > 0 and ccount < 100): hist[4][ccount] = hist[4][ccount] + 1
+            if (ccount > 0 and ccount < 100): hist[6][ccount] = hist[6][ccount] + 1
         dTE = dTL = extselTL = extselTH = -1
         ccount = 0
     else:
         ccount = ccount + 1
-    if lastLine[extsel] == 1 and line[extsel] == 0: extselTL = ccount
-    if lastLine[extsel] == 0 and line[extsel] == 1: extselTH = ccount
-    if line[extsel] == 0:
-        #if lastLine[d0] != line[d0]: dTL = ccount
-        #if dTE == -1 and lastLine[d0] != line[d0]: dTE = ccount
-        if lastLine[d0] != line[d0]: hist[2][ccount] = hist[2][ccount] + 1
+    if [lineNo > 2]:
+        if lastLine[extsel] == 1 and line[extsel] == 0: extselTL = ccount
+        if lastLine[extsel] == 0 and line[extsel] == 1: extselTH = ccount
+        if lastLine[d0] != line[d0] or lastLine[d1] != line[d1]: 
+            if line[rw] == 1: 
+                if line[extsel] == 0:
+                    hist[2][ccount] = hist[2][ccount] + 1
+                else:
+                    hist[3][ccount] = hist[3][ccount] + 1
+            else:
+                hist[4][ccount] = hist[4][ccount] + 1
+        if lastLine[a0] != line[a0] or lastLine[a1] != line[a1] or lastLine[a2] != line[a2] or lastLine[a3] != line[a3]:
+            hist[5][ccount] = hist[5][ccount] + 1
     #else:
     #   if lastLine[d0] != line[d0]: hist[3][ccount] = hist[3][ccount] + 1
     #if lastLine[d0] != line[d0] and ccount < 2: print ("data change at ccount %d line %d" % (ccount, lineNo))
@@ -46,5 +59,5 @@ for line in d:
 lineNo = 0
 for line in hist.T:
     if np.sum(line) > 0:
-        print("%3d %6d %6d %6d %6d %6d" % (lineNo, line[0], line[1], line[2], line[3], line[4]))
+        print("%3d %6d %6d %6d %6d %6d %6d %6d" % (lineNo, line[0], line[1], line[2], line[3], line[4], line[5], line[6]))
     lineNo = lineNo + 1
--- tools/busa.sh
+++ tools/busa.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname $0)/.."
+. ./tools/config
 LA_DIR=/home/jim/opt/Dall-in-one_6.0.0.1-linux-x64.zip/
 TAG=`date +%Y%m%d.%H%M%S`
 OUT=./traces/${TAG}
@@ -9,7 +10,7 @@ git diff > ${OUT}.git_diff
 echo "$(git describe --abbrev=6 --dirty --always)" >> ${OUT}.git_version
 GIT="$(git describe --abbrev=6 --dirty --always)"
 
-${LA_DIR}/TerminalCapture capture /dev/serial/by-id/usb-Dr._Gusman*  tools/cap.tcs /tmp/tc.csv \
+${LA_DIR}/TerminalCapture capture ${BUSA} tools/cap24.tcs /tmp/tc.csv \
 	&& cat /tmp/tc.csv \
 	| tr ',' ' ' \
 	| tail -n +2  > ${OUT}.cap
