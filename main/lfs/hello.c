e2589896451f3b77e5850f17470ddb32eb34bd93
jim@vheavy.com <jim@vheavy.com>
2025-10-15 15:22:57 -0700

    hello.c

--- main/lfs/hello.c
+++ main/lfs/hello.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
-uint8_t *portb = (uint8_t *)0xd301;
-uint8_t *cartA = (uint8_t *)0xa000;
+volatile uint8_t *portb = (uint8_t *)0xd301;
+volatile uint8_t *cartA = (uint8_t *)0xa000;
 
 int main(void) {
   while(1) { 
 	FILE *f = fopen("D1:LLVMOUT.TXT", "w");
   	fprintf(f, "TEST");
   	fclose(f);
-	uint8_t oldPortb = *portb;
-	uint8_t basicA000 = *cartA;
-	*portb = 0xff;
-	uint8_t nobasA000 = *cartA;
-	*portb = oldPortb;
-        printf("oldPortB = %02x A000 = %02x/%02x\n", oldPortb, basicA000, nobasA000);
+
+	*portb = 0xff; // TURN OFF BASIC
+	*cartA = 0xee;
+
+	uint8_t oldPortb = *portb, basicA000, nobasA000;
+	for (long n = 0; n < 1000; n++) { 
+		basicA000 = *cartA;
+		*portb = 0xff; // TURN OFF BASIC
+		nobasA000 = *cartA;
+		if (nobasA000 != 0xee) { printf("0xa000 != 0xee!!!\n"); }
+		*cartA = 0xee;
+		*portb = 0xfd; // TURN ON BASIC
+		*cartA = 0x11; // mem write should be ignored
+
+	}
+    printf("oldPortB = %02x A000 = %02x/%02x\n", oldPortb, basicA000, nobasA000);
   }
   return 0;
 }
