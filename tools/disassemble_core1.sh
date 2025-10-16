88b5cc53b1c31ba2771d8f049d0d049402c6f444
jim@vheavy.com <jim@vheavy.com>
2025-10-16 13:41:33 -0700

    make disassemble_core.sh leave output files with git branch and version

--- tools/disassemble_core1.sh
+++ tools/disassemble_core1.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -x
+cd "$(dirname $0)/.."
+
 PYVER=3.12
 if [ "$(hostname)" == "miner6" ]; then PYVER=3.13; fi
+GIT="$(git describe --abbrev=6 --dirty --always)"
+BRANCH="$(git branch --show-current)"
+
 
 printf "set pagination off\n disass /s iloop_pbi"  \
         | /home/jim/.espressif/tools/xtensa-esp-elf-gdb/16.2_20250324/xtensa-esp-elf-gdb/bin/xtensa-esp-elf-gdb-$PYVER  ./build/atari8EspBusBang.elf \
-        2> /dev/null
+        2> /dev/null \
+	| tee ./stash/core1.${BRANCH}.${GIT}.lst
+
+cp ./stash/core1.${BRANCH}.${GIT}.lst ./stash/core1.${BRANCH}.lst
 
 
