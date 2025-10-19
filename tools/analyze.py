9c0106803aebcd81f7c175c8c8d068d1111bf248
Jim Evans <jim@vheavy.com>
2025-10-19 05:48:02 -0700

    Change to use the same busa asm program as master.  Change analyzer.py to track RW line

--- tools/analyze.py
+++ tools/analyze.py
@@ -23,7 +23,7 @@ extselTH = -1
 dTE = -1 
 dTL = -1
 
-hist = np.zeros((11,100))
+hist = np.zeros((12,100))
 lineNo = 0
 maxCyc = 0
 for line in d:
@@ -59,13 +59,14 @@ for line in d:
         if lastLine[ready] != line[ready]: hist[7][ccount] = hist[7][ccount] + 1
         if lastLine[extdec] != line[extdec]: hist[8][ccount] = hist[8][ccount] + 1
         if lastLine[refresh] != line[refresh]: hist[9][ccount] = hist[9][ccount] + 1
+        if lastLine[rw] != line[rw]: hist[11][ccount] = hist[9][ccount] + 1
     #else:
     #   if lastLine[d0] != line[d0]: hist[3][ccount] = hist[3][ccount] + 1
     #if lastLine[d0] != line[d0] and ccount < 2: print ("data change at ccount %d line %d" % (ccount, lineNo))
     lastLine = line
     lineNo = lineNo + 1
 
-print("     EXTSL  EXTSH   DESL   DESH   DWR    ADDR    CLK  READY  EXDEC    REF   DREF")
+print("     EXTSL  EXTSH   DESL   DESH   DWR    ADDR    CLK  READY  EXDEC    REF   DREF      RW")
 lineNo = 0
 for line in hist.T:
     if lineNo <= maxCyc or np.sum(line) > 0:
