bbc54bb1926e71edbbefb8f399e0a807573317b6
Jim Evans <jim@vheavy.com>
2025-12-12 06:38:31 -0800

    leadin nops 3->4

--- main/core1.cpp
+++ main/core1.cpp
@@ -71,7 +71,7 @@ void iloop_pbi() {
         bmonHead = (bHead + 1) & bmonArraySzMask;
         //uint32_t pinEnMask = pinEnableMask;
         //uint32_t pinDrMask = pinDriveMask;
-        AsmNops<3>::generate(); 
+        AsmNops<4>::generate(); 
 
         // Timing critical point #1: >= 17 ticks after clock edge until read of address/control lines
         r0 = REG_READ(GPIO_IN_REG);
@@ -109,7 +109,7 @@ void iloop_pbi() {
                 mmuState.banks[bank40] = mmuState.extBanks[extMemBank];
                 
                 //AsmNops<25>::generate(); // about this much free time remains here 
-                while(XTHAL_GET_CCOUNT() - tscFall < 100) {}
+                while(XTHAL_GET_CCOUNT() - tscFall < 95) {}
                 REG_WRITE(GPIO_ENABLE1_W1TC_REG, pinReleaseMask);
                 PROFILE4(XTHAL_GET_CCOUNT() - tscFall);// 112-120 cycles seems to be the limits  // 
         } else {
@@ -119,7 +119,7 @@ void iloop_pbi() {
                 mmuState.banks[bank80] = mmuState.basicEnBankMux[(d000Write[_0x301] >> 1) & 0x1];
                 AsmNops<0>::generate(); 
                  
-                while(XTHAL_GET_CCOUNT() - tscFall < 80) {}
+                while(XTHAL_GET_CCOUNT() - tscFall < 100) {}
                 uint32_t r1 = REG_READ(GPIO_IN1_REG);
                 PROFILE3(XTHAL_GET_CCOUNT() - tscFall);
                 data = (r1 >> bus.data.shift);
